35c840260477eafda6c1df87f6f838678ba17e9b
AbdallahM19 <abdullahmaabakar@gmail.com>
2026-07-25 00:20:49 +0200

    creating exceptions module

---
+++ src/vaultops/exceptions.py
@@ -0,0 +1,26 @@
+"""vaultops exceptions module"""
+
+
+class VaultOpsError(Exception):
+    """The Base Exception"""
+
+    def __init__(self, message: str) -> None:
+        super().__init__(message)
+        self.message = message
+
+
+class ValidationError(VaultOpsError):
+    """Validation Error Exception"""
+
+
+class StorageError(VaultOpsError):
+    """Storage Error Exception"""
+
+
+class DuplicateEntryError(StorageError):
+    """Duplicate Entry Error Exception"""
+
+
+class EntryNotFoundError(StorageError):
+    """Entry Not Found Error Exception"""
+
