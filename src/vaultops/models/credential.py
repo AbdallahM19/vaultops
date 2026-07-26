d7c27e360f4d47d4c3445b78b17467b674237942
AbdallahM19 <abdullahmaabakar@gmail.com>
2026-07-26 09:04:55 +0200

    add __post_init__() method

--- src/vaultops/models/credential.py
+++ src/vaultops/models/credential.py
@@ -2,6 +2,7 @@
 
 from dataclasses import dataclass
 
+from vaultops.exceptions import ValidationError
 from vaultops.models.base import BaseEntry
 
 
@@ -11,3 +12,14 @@ class Credential(BaseEntry):
     username: str
     password: str
 
+    def __post_init__(self) -> None:
+        super().__post_init__()
+
+        if self.service_name == "":
+            raise ValidationError("service_name must be a non-empty string")
+
+        if self.username == "":
+            raise ValidationError("username must be a non-empty string")
+
+        if len(self.password) < 8:
+            raise ValidationError("password must be at least 8 characters")
